5be667eac6cf5187cf8c7a9e2e4e6f9ba49e034c
GeorgeVillarroel <george.dan.villarroel@gmail.com>
2024-11-20 10:23:35 -0300

    debug the parser

--- hello.c
+++ hello.c
@@ -25,27 +25,30 @@ void parser(char (*str)[30], char newStr[]) {
     strcpy(str[0], "call");
 
 
-    int k = 5;
-    for ( k ; newStr[k] != '\0' && newStr[k] != '\r' && newStr[k] != '\n' && newStr[k] != ' ' ; k = k +1) {
+    int j = 0;
+    for ( int k = 5 ; newStr[k] != '\0' && newStr[k] != '\r' && newStr[k] != '\n' && newStr[k] != ' ' ; k = k +1) {
       if (isalpha(newStr[k]) || newStr[k] == '.' || isdigit(newStr[k]) || newStr[k] == '_' ){
-	str[1][k-5] = newStr[k];
+	str[1][j] = newStr[k];
       }
+      j = j + 1;
     }
 
     // Sometimes in debugging it would add an inverted ? at the \0 index
-    str[1][k-5] = '\0';
+    str[1][j] = '\0';
 
   } else if(newStr[0] == 'u' && newStr[1] == 's' &&  newStr[2] == 'e' && newStr[3] == ' ') {
     strcpy(str[0], "use");
 
-    int j = 4;
-    for ( j ; newStr[j] != '\0' && newStr[j] != '\r' && newStr[j] != '\n' && newStr[j] != ' ' ; j = j +1) {
-      if (isalpha(newStr[j] || newStr[j] == '.' ) || isdigit(newStr[j] || newStr[j] == '_' ) ){
-	str[1][j-4] = newStr[j];
+
+    int j = 0;
+    for ( int k = 4 ; newStr[k] != '\0' && newStr[k] != '\r' && newStr[k] != '\n' && newStr[k] != ' ' ; k = k +1) {
+      if ( isalpha(newStr[k]) || newStr[k] == '.' || isdigit(newStr[k]) || newStr[k] == '_') {
+	str[1][j] = newStr[k];
       }
+      j = j + 1;
     }
 
-    str[1][j-4] = '\0';
+    str[1][j] = '\0';
 
   } else {
     printf("Syntax Error: %s . Not a proper call.\n", newStr);
