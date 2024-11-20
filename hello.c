c31deea3721fe23981c070f1ed1e329905260fa5
GeorgeVillarroel <george.dan.villarroel@gmail.com>
2024-11-20 05:32:07 -0300

    debugging parser

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // \\ []{}
 
@@ -22,8 +23,21 @@ int sizeCalculator(char nameFile[]) {
 void parser(char (*str)[30], char newStr[]) {
   if (newStr[0] == 'c' && newStr[1] == 'a' &&  newStr[2] == 'l' && newStr[3] == 'l' && newStr[4] == ' '){ 
     strcpy(str[0], "call");
+
+
   } else if(newStr[0] == 'u' && newStr[1] == 's' &&  newStr[2] == 'e' && newStr[3] == ' ') {
     strcpy(str[0], "use");
+
+    int j = 4;
+    for ( j ; newStr[j] != '\0' && newStr[j] != '\r' && newStr[j] != '\n' && newStr[j] != ' ' && newStr[j] != '�' ; j = j +1) {
+      if (isalpha(newStr[j]) || newStr[j] == '.'){
+	str[1][j-4] = newStr[j];
+      }
+    }
+
+
+  printf("%s %i \n", str[1], j);
+
   } else {
     printf("Syntax Error: %s . Not a proper call.\n", newStr);
   }
@@ -40,13 +54,12 @@ int main(int argc, char *argv[]) {
   FILE *scriptFile;
   scriptFile = fopen(argv[1], "r");
 
-  char scriptArray[arraySize];
+  char scriptArray[50];
   char commandString[2][30];
 
-  while (fgets(scriptArray, arraySize, scriptFile)) {;
+  while (fgets(scriptArray, 50, scriptFile)) {;
     // printf("%s",scriptArray);
-    parser(commandString, scriptArray);
-    printf("%s",commandString[0]);
+    parser(commandString, scriptArray); 
   }
 
   fclose(scriptFile);
